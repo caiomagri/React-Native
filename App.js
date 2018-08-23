564966bb1478bdc23b8ff19ab522d9227427e081
Caio Magri <kaioeduardofm@gmail.com>
2018-08-22 22:23:11 -0300

    [FIX] First Commit for this project

---
+++ App.js
@@ -0,0 +1,26 @@
+import { createStackNavigator } from "react-navigation";
+import LoginPage from './src/pages/LoginPage'
+
+export default createStackNavigator({
+	Login: {
+		screen: LoginPage,
+		navigationOptions: {
+			title: 'Bem Vindo'
+		}
+	}
+}, {
+	navigationOptions: {
+		title: "Series!",
+		headerTintColor: 'white',
+		headerStyle: {
+			backgroundColor: '#6ca2f7',
+			borderBottomWidth: 1,
+			borderBottomColor: '#C5C5C5'
+		},
+		headerTitleStyle: {
+			color: 'white',
+			fontSize: 30
+		}
+	}
+}
+);
